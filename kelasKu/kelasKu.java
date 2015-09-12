9a68b025f72bb6b339db163f5a0abee695990bff
fajri28 <fajri28mhd@gmail.com>
2015-09-12 17:13:10 +0700

    Prek-05

---
+++ kelasKu/kelasKu.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class kelasKu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class kelasKu
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor =hal yang pertama kali dilakukan for objects of class kelasKu
+     */
+    public kelasKu()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
